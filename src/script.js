65f42220530da0f02e7d9ddc3550f23b8bd76bb7
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-12-29 21:05:58 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+ import Email from 'https://smtpjs.com/v3/smtp.js';
 
 
 const header = document.querySelector("haeder");
@@ -55,9 +55,9 @@ function validateForm(e) {
     e.preventDefault();
 
     validateInputs();
-    // formContainer.classList.add('hidden');
-    // successMessage.classList.remove('hidden');
     sendEmail();
+    formContainer.classList.add('hidden');
+    successMessage.classList.remove('hidden');
 }
 
 // submitNextBtn.addEventListener('click', () => {
@@ -66,8 +66,6 @@ function validateForm(e) {
 // })
 
 function sendEmail() {
-    
-    var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 
     Email.send({
