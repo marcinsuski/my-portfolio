0e364292d55b4120109a5f264cf2a515d10b75cb
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-12-30 13:13:50 +0100

    hamburger menu for mobile

--- src/script.js
+++ src/script.js
@@ -225,3 +225,13 @@ mailContent.addEventListener("change", () => {
         sibling.classList.remove('active');
     }
 });
+
+
+// toggle mobile menu
+
+
+const navToggle = document.querySelector('.nav__toggle');
+
+navToggle.addEventListener('click', () => { 
+    document.body.classList.toggle('nav-open');
+})
