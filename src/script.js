a5d3ad55e200b0c7ebb08b82ce46c036c82b8168
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-12-30 13:19:44 +0100

    .

--- src/script.js
+++ src/script.js
@@ -232,6 +232,7 @@ mailContent.addEventListener("change", () => {
 
 const navToggle = document.querySelector('.nav__toggle');
 
+
 navToggle.addEventListener('click', () => { 
     document.body.classList.toggle('nav-open');
 })
